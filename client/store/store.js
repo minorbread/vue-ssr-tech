342c89a0a7045d0d143842495ae625a6ac597bf3
minorbread <hmcdl@foxmail.com>
2018-06-09 21:06:45 +0800

    vuex add hot replace

--- client/store/store.js
+++ client/store/store.js
@@ -7,7 +7,7 @@ import actions from './actions/actions'
 const isDev = process.env.NODE_ENV === 'development'
 
 export default () => {
-  return new Vuex.Store({
+  const store = new Vuex.Store({
     strict: isDev,
     state: defaultState,
     mutations,
@@ -50,4 +50,26 @@ export default () => {
       }
     }
   })
+
+  if (module.hot) {
+    module.hot.accept([
+      './state/state',
+      './mutations/mutations',
+      './getters/getters',
+      './actions/actions'
+    ], () => {
+      const newState = require('./state/state').default
+      const newMutations = require('./mutations/mutations').default
+      const newGetters = require('./getters/getters').default
+      const newActions = require('./actions/actions').default
+
+      store.hotUpdate({
+        state: newState,
+        mutations: newMutations,
+        getters: newGetters,
+        actions: newActions
+      })
+    })
+  }
+  return store
 }
