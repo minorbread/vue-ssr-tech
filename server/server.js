ad3a515e4521857d2357f4981941578b3379e25d
minorbread <hmcdl@foxmail.com>
2018-06-11 21:22:45 +0800

    production ssr

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ const Koa = require('koa')
 const send = require('koa-send')
 const path = require('path')
 
+const staticRouter = require('./routers/static')
+
 const app = new Koa()
-const pageRouter = require('./routers/dev-ssr.js')
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -30,6 +31,16 @@ app.use(async (ctx, next) => {
     await next()
   }
 })
+
+app.use(staticRouter.routes()).use(staticRouter.allowedMethods())
+
+let pageRouter
+
+if (isDev) {
+  pageRouter = require('./routers/dev-ssr')
+} else {
+  pageRouter = require('./routers/ssr')
+}
 
 app.use(pageRouter.routes()).use(pageRouter.allowedMethods())
 
