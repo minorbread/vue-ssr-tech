cf9fcbd4be11e8704f97b0ef5ceb7376d62de04e
minorbread <hmcdl@foxmail.com>
2018-06-12 15:40:33 +0800

    add server Api request base

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
-module.exports {
+module.exports = {
   db: {
     appId: 'A6073221669886',
-    appKey: '5D3AB0E7-F4FB-B899-5BB8-DD3B31B63958'
+    appKey: '878SGADA9S-F4FB-B899-5BB8-878SGADA9S'
   }
 }
